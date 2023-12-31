7ae29fadedcff4b3ece6465af479c1c9e7fee40d
ma-boles <ma_boles@hotmail.com>
2023-12-30 18:06:06 -0800

    git commit -m "added tailwind"

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -4,8 +4,8 @@ import Links from "./links/Links";
 export default function NavBar () {
     return (
         <>
-        <div>
-            <div>Logo</div>
+        <div className="flex justify-between pt-6 pb-6">
+            <div className="pl-2 text-xl font-bold">Logo</div>
             <div>
                 <Links />
             </div>

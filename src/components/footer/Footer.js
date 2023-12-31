5450e6858bb7231a94c768f94c5d55037971756b
ma-boles <ma_boles@hotmail.com>
2023-12-30 18:02:18 -0800

    git commit -m "added copyright + year"

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,10 @@
 export default function Footer () {
     return (
         <>
-        <div>Footer</div>
+        <div>
+            <hr className="pb-4"/>
+            <h2 className="pr-2 pb-2 text-right">&copy; mab.dev 2024</h2>
+        </div>
         </>
     )
 }

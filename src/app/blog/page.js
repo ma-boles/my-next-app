482f7c2534f81f8029530a013c182a4d3fd1372d
ma-boles <ma_boles@hotmail.com>
2024-01-08 17:11:21 -0800

    git commit -m "added data fetch logic"

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,23 +1,35 @@
+import React from "react";
 import styles from "../blog/blog.module.css";
 import PostCard from "@/components/postcard/postCard";
+import { getPosts } from "@/lib/data";
 
-export default function BlogPage () {
+/*const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    if(!res.ok) {
+        throw new Error("Something went wrong");
+    }
+    return res.json();
+}*/
+
+const BlogPage = async () => {
+
+    /*const posts = await getData()*/
+
+    const posts = await getPosts();
     return (
         <>
-        <div className={styles.container }>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
+        <div className={styles.container}>
+            {posts.map((post) => 
+                <div className={styles.post} key={post.id}>
+                    <PostCard post={post}/>
+                </div>
+            )}
+            
+            
         </div>
         </>
     )
 }
+
+export default BlogPage;
--- src/components/postcard/postCard.js
+++ src/components/postcard/postCard.js
@@ -1,20 +1,22 @@
 import styles from "./postCard.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function PostCard () {
+export default function PostCard ({post}) {
     return (
         <>
         <div className={styles.container}>
             <div className={styles.top}>
                 <div className={styles.imageContainer}>
-                    <h1>image</h1>
+                                   
                 </div>
                 <span className={styles.date}>01.01.2024</span>
             </div>
             <div className={styles.bottom}>
-                <h1>Title</h1>
-                <p>Desc</p>
+                <h1 className="text-lg font-bold">{post.title}</h1>
+                <p className="text-sm text-slate-500">{post.body}</p>
                 <p>READ MORE</p>
+                
             </div>
         </div>
         

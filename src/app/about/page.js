fd77fba06bc3b02c5bd34c06667cda336f38e0bc
ma-boles <ma_boles@hotmail.com>
2024-01-09 16:56:25 -0800

    git commit -m "updated text"

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,8 +5,9 @@ export default function AboutPage () {
         <>
         <div className={styles.container }>
             <div className={styles.textContainer}>
-                <h1 className="text-4xl font-bold text-slate-500">About Initiative</h1>
-                <p className="font-5xl">Open source projects awaiting your valuable input. Open to all levels!</p>
+                <h1 className="text-4xl font-bold text-slate-500">About</h1>
+                <p className="font-5xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
                 <div className={styles.box}>
                     <div>
                         <div className="text-slate-500">3</div>

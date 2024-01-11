2edbadcb3858ef1f05c421d08816b6f3cb20a5b8
ma-boles <ma_boles@hotmail.com>
2024-01-10 17:33:48 -0800

    git commit -m "added image import"

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,15 +1,17 @@
 import styles from "./singlePost.module.css";
+import Image from "next/image";
+
 
 export default function SinglePostPage () {
     return (
         <>
         <div className={styles.container}>
             <div className={styles.imageContainer}>
-                Image
+               image
             </div>
             <div className={styles.textContainer}>
-                <h1>Title</h1>
-                <p>Desc</p>
+                <h1>title</h1>
+                <p>body</p>
             </div>  
         </div>
         </>

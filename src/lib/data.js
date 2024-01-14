7f07cf0a347200cafc56a3c0eef73cdd8cf0026f
ma-boles <ma_boles@hotmail.com>
2024-01-13 17:36:25 -0800

    git commit -m "fixed misspelling on error message"

--- src/lib/data.js
+++ src/lib/data.js
@@ -11,14 +11,14 @@ export const getPosts =  async () => {
     }
     catch(err) {
         console.log(err);
-        throw new Error("Failed to fetch post");
+        throw new Error("Failed to fetch posts");
     }
 };
 
 export const getPost = async (slug) => {
     try {
         connectToDB();
-        const post = await Post.findOne({slug});
+        const post = await Post.find({slug});
         return post;
     } catch (err) {
         console.log(err);

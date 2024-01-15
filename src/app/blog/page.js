820bb70b88b8a1c3ddd685a85be326ff4a556d79
ma-boles <ma_boles@hotmail.com>
2024-01-14 17:26:54 -0800

    git commit -m "added ()"

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -20,16 +20,14 @@ const BlogPage = async () => {
     return (
         <>
         <div className={styles.container}>
-            {posts.map((post) => 
+            {posts.map((post) => (
                 <div className={styles.post} key={post.id}>
                     <PostCard post={post}/>
                 </div>
-            )}
-            
-            
+            ))}
         </div>
         </>
-    )
-}
+    );
+};
 
 export default BlogPage;
